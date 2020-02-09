e817639dc5d8c4b4411d378f92910c026ced9b39
aliandfatma <aliatliandfatmadurak@gmail.com>
2020-02-09 17:44:02 +0300

    Updates

---
+++ precache-manifest.1069d03d7d08c31561dbb4973808f5ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660a3de1a61462c767a0dcb7bf8e6245",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "e32c3a6dc670cc4ed7cf",
+    "url": "/birthday/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "667127c39a825281c3cf",
+    "url": "/birthday/static/js/2.79fb78ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/birthday/static/js/2.79fb78ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32c3a6dc670cc4ed7cf",
+    "url": "/birthday/static/js/main.94de1574.chunk.js"
+  },
+  {
+    "revision": "4786df56ccda1ed22244",
+    "url": "/birthday/static/js/runtime-main.c3ad665b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/birthday/static/media/logo.5d5d9eef.svg"
+  }
+]);
